530a713d500bc976ae2e1491036480799498c064
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-03-26 14:03:55 +0800

    add String Module Folder  - add VidPartOfStrings.py

---
+++ Week4/Strings/VidPartOfStrings.py
@@ -0,0 +1,24 @@
+name = "Jaylen"
+print(name[1])
+# print a
+
+print(name[0])
+#print j
+
+# print last character
+print(name[-1])
+# so print n
+# it used for long text that you did know the length
+
+color = "Orange"
+print(color[1:4])
+# print ran
+
+# How about print forward
+# Example
+fruit = "Pineapple"
+print(fruit[4:])
+# print apple
+
+print(fruit[:4])
+#print pine
