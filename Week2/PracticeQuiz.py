d70b02143aa321dc1731416f9422358e4ecc924b
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-03-26 13:21:33 +0800

    add PracticeQuiz.py

---
+++ Week2/PracticeQuiz.py
@@ -0,0 +1,24 @@
+# This code is supposed to take two numbers, divide one by another so that the result is equal to 1,
+# and display the result on the screen. Unfortunately, there is an error in the code.
+# Find the error and fix it, so that the output is correct.
+
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(result)
+
+
+# Combine the variables to display the sentence "How do you like Python so far?"
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+
+print(word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " " + word6 + " " + word7)
+
+# This code is supposed to display "2 + 2 = 4" on the screen, but there is an error. Find the error in the code and fix it, so that the output is correct.
+print("2 + 2 = " + str(2 + 2))
+
