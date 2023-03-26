afde33b8047f93d6e02a0d062034849f15371436
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-03-26 13:14:32 +0800

    Add Week 2 and Add Coding skills

---
+++ Week2/CodingSkills.py
@@ -0,0 +1,58 @@
+# Skills Group 1
+# The following lines assign the variable to the left of the =
+# assignment operator with the values and arithmetic expressions
+# on the right side of the = assignment operator.
+hotel_room = 100
+tax = hotel_room * 0.08
+total = hotel_room + tax
+room_guests = 4
+share_per_person = total / room_guests
+
+# This line outputs the result of the final calculation stored
+# in the variable "share_per_person"
+print("Each person needs to pay: " + str(share_per_person))  # change a data type
+
+# Skills Group 2
+# The following 5 lines assign strings to a list of variables.
+salutation = "Dr."
+first_name = "Prisha"
+middle_name = "Jai"
+last_name = "Agarwal"
+suffix = "Ph.D."
+
+print(salutation + " " + first_name + " " + middle_name + " " + last_name + ", " + suffix)
+# The comma as a string ", " adds the conventional use of a comma plus a
+# space to separate the last name from the suffix.
+
+# Alternatively, you could use commas in place of the + connector:
+print(salutation, first_name, middle_name, last_name, ",", suffix)
+# However, you will find that this produces a space before a comma within a string.
+
+# skills Group 3
+# Resolve TypeError caused by a data type mismatch issue
+# Use an explicit conversion function
+
+# print("5 * 3 = " + (5 * 3))
+
+# Resolution:
+#print("5 * 3 = " + str(5*3))
+#
+# To avoid a type error between the string and the integer within the
+# print() function, you can make an explicit data type conversion by
+# using the str() function to convert the integer to a string.
+
+
+# skill group 4
+#Resolve a ZeroDivisionError caused by an attempt to divide by 0
+
+numerator = 7
+denominator = 0  # Possible resolution: Change the denominator value
+result = numerator / denominator
+print(result)
+
+# One possible assumption for a number divided by zero error might
+# include the issue of a null value as a denominator (could happen when
+# using a loop to iterate over values in a database). In such cases, the
+# desired outcome may be to leave the numerator value intact. The
+# numerator value can be preserved by reassigning the denominator with
+# the integer value of 1. The result would then equal the numerator.
