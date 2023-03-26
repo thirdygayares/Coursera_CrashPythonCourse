9ef944c1104cf52f36781d46c3ed648f12843ba7
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-03-26 14:23:00 +0800

    add ReadStringIndexingAndSlicing.py

---
+++ Week4/Strings/ReadStringIndexingAndSlicing.py
@@ -0,0 +1,14 @@
+fruit = "Mangosteen"
+print(fruit[1:4])
+# print ang
+
+print(fruit[:5])
+# print Mango
+
+print(fruit[5:])
+# print steen
+
+# You might have noticed that if you put both of those results together, you get the original string back!
+print(fruit[:5] + fruit[5:])
+# print Mangosteen
+
