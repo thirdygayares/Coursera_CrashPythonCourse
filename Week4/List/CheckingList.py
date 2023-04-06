f9ef917b538cf7617589875d676afe7bf6040629
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-04-06 21:38:15 +0800

    add List Folders add CheckingList.py

---
+++ Week4/List/CheckingList.py
@@ -0,0 +1,18 @@
+# checking type
+x = ["Now", "we", "are", "cooking"]
+print(type(x))
+
+# print list
+print(x)
+
+# know the lenght of the list
+print(len(x))
+
+# check if the element is in the list
+print("are" in x) #true
+print("Today" in x) # false
+
+# print specific element
+print(x[0])
+print(x[1])
+print(x[1:3])
