a9f514e87140e70c9b0c42c8d67343949fef13ef
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-04-06 21:49:59 +0800

    add ModifyList.py

---
+++ Week4/List/ModifyList.py
@@ -0,0 +1,17 @@
+# Create Array
+fruits = ["Pineapple", "Banana", "Apple", "Melon"]
+# add an element
+fruits.append("Kiwi")
+print(fruits)
+# add an element on specific location
+fruits.insert(0, "Orange")
+print(fruits)
+# Remove in elements
+fruits.remove("Banana")
+print(fruits)
+# Remove by pop
+fruits.pop(2)
+print(fruits)
+# Change element or edit
+fruits[0] = "Mango"
+print(fruits)
